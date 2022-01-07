0383024ecb207469a62c2bc708f214e06dba2142
Wagner Rigoli da Rosa <wagner@rigolidarosa.com>
2022-01-07 11:26:07 -0300

    Desafio contador

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,54 @@
-console.log("Hello World!");
+var currentNumberWrapper = document.getElementById('currentNumber');
+
+var plus = document.getElementById('plus');
+plus.addEventListener('click', increment, false);
+
+var less = document.getElementById('less');
+less.addEventListener('click', decrement, false);
+
+var currentNumber = 0;
+const limitUp = 10;
+const limitDown = -10;
+
+function increment() {
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+
+    less.disabled = false;
+
+    if (currentNumber < 0) {
+        addNegativeClass();
+    } else if (currentNumber < 0) {
+        rmNegativeClass()
+    }
+
+    if (currentNumber === limitUp) {
+        plus.disabled = true;
+    }
+}
+
+function decrement() {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+
+    plus.disabled = false;
+
+    if (currentNumber < 0) {
+        addNegativeClass();
+    } else if (currentNumber < 0) {
+        rmNegativeClass()
+    }
+
+    if (currentNumber === limitDown) {
+        less.disabled = true;
+    }
+}
+
+
+function addNegativeClass() {
+    currentNumberWrapper.classList.add('negative');
+}
+
+function rmNegativeClass() {
+    currentNumberWrapper.classList.remove('negative');
+}
